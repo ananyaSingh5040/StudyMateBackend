32a92e06fbeb2385643105c31c4b6dfd8468312d
Ananya Singh <singhananya5040@gmail.com>
2025-06-23 15:08:11 +0530

    routes

---
+++ routes/plannerRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const {getTasks, createTask, updateTask, deleteTask} = require("../controllers/plannerController");
+const router = express.Router();
+router.get("/", getTasks);
+router.post("/",createTask);
+router.put("/:id",updateTask);
+router.delete("/:id",deleteTask);
+module.exports = router;
