2506efc05fcb0fb03556718dd5ba82c1b77a55b2
Ananya Singh <singhananya5040@gmail.com>
2025-06-23 15:08:01 +0530

    model

---
+++ models/Task.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const TaskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+module.exports = mongoose.model("Task",TaskSchema);
