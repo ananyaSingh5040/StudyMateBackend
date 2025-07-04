c3c2ff8638cb46ce21a21ce49dfb44e04fa21eff
Ananya Singh <singhananya5040@gmail.com>
2025-07-04 13:42:49 +0530

    index

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-// SERVER
-const express = require("express");
-const cors = require("cors");
-const connectDB = require("./config");
-require("dotenv").config();
-const app = express();
-const taskRoutes = require("./routes/plannerRoutes");
-const doubtRoutes = require("./routes/doubtRoutes");
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config.js"; // make sure your DB file is renamed to db.js
+import plannerRoutes from "./routes/plannerRoutes.js";
+import doubtRoutes from "./routes/doubtRoutes.js";
 
-// middlewares
+dotenv.config();
+const app = express();
+
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
-// testing
+// Test Route
 app.get("/", (req, res) => {
   res.send("Backend is live!");
 });
 
-app.use("/api/planner",taskRoutes);
-app.use("/api/doubt",doubtRoutes);
-// connecting DB
+// API Routes
+app.use("/api/planner", plannerRoutes);
+app.use("/api/doubt", doubtRoutes);
+
+// Connect to DB
 connectDB();
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running at port: ${PORT}`);
